8881f325b1516a54d6860ef54ef215fc8c589adb
Patrik Alm <almpatrik@yahoo.se>
2025-01-21 11:06:17 +0100

    Part 1 initializing done

---
+++ src/main/java/io/github/patrikalm/g52springlibraryworkshop/G52SpringLibraryWorkshopApplication.java
@@ -0,0 +1,15 @@
+package io.github.patrikalm.g52springlibraryworkshop;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class G52SpringLibraryWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G52SpringLibraryWorkshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/patrikalm/g52springlibraryworkshop/G52SpringLibraryWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.patrikalm.g52springlibraryworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G52SpringLibraryWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
